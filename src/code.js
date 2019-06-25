a3ed123cbd246aecc97d0feae9f476e8a948adae
Andrey Rogachevich <andrey.rogachevich@azati.com>
2019-06-26 01:14:45 +0300

    5. bind

--- src/code.js
+++ src/code.js
@@ -1,1 +1,19 @@
-// EVENT in HTML
+// call, bind
+var make = "Mclaren";
+var model= "720s";
+function fullName(num){
+	console.log(`${num} ${this.make} ${this.model}`);
+}
+var car = {
+	make:"Lamborghini",
+	model:"Huracán",
+};
+
+fullName.call(car, 1);
+
+fullName.apply(car, [2]);
+
+var func = fullName.bind(car);
+func(3);
+
+fullName(4);
