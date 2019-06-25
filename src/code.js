898f8ee85ae72d0ed88d4f640d00bf0bb0939e4b
Andrey Rogachevich <andrey.rogachevich@azati.com>
2019-06-26 01:03:14 +0300

    3. alone

--- src/code.js
+++ src/code.js
@@ -1,16 +1,4 @@
-// WARN : js
+// ALONE
 var make = "Mclaren";
 var model= "720s";
-function fullName(){
-	console.log(`${this.make} ${this.model}`);
-}
-var car = {
-	make:"Lamborghini",
-	model:"Huracán",
-	fullName:function () {
-		console.log(`${this.make} ${this.model}`);
-	}
-};
-car.fullName();
-window.fullName();
-fullName();
+console.log(`${this.make} ${this.model}`);
