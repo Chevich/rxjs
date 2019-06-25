c217c1122d3a3b1ed8bc3b4894372568b2dbff78
Andrey Rogachevich <andrey.rogachevich@azati.com>
2019-06-26 00:36:02 +0300

    1.sugar

--- src/code.ts
+++ src/code.ts
@@ -1,27 +1,9 @@
-import { fromEvent, of } from 'rxjs';
-import { bufferTime, catchError, concatMap, filter, retry } from 'rxjs/operators';
-import { fromPromise } from 'rxjs/internal-compatibility';
-
-const a$ = fromEvent(document, 'click')                                                   // событие от клиента
-  .pipe(
-    bufferTime(1500),
-    filter(x => x.length),
-    concatMap(clicks => of(clicks).pipe(
-      concatMap(clicks => fromPromise(Promise.resolve('pizza, '.repeat(clicks.length)))), // assume long operation
-      concatMap(pack => fromPromise(Promise.resolve(`delivery (${pack})`))),              // assume long operation
-      retry(3),
-      ),
-    ),
-    catchError(() => 'We\'ve lost client and his money'),
-  )
-;
-
-a$.subscribe(parcel => addItem(`Customer have got ${parcel}`));                           // Заказчик доволен
-
-
-function addItem(val: any) {
-  const node = document.createElement('li');
-  const textnode = document.createTextNode(val);
-  node.appendChild(textnode);
-  document.getElementById('output').appendChild(node);
-}
+const car = {
+	make: "Lamborghini",
+	model: "Huracán",
+	fullName: function () {
+		console.log(`${this.make} ${this.model}`);
+		console.log(`${car.make} ${car.model}`);
+	}
+};
+car.fullName();
