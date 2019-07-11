dde97a846f06c5ea99ce73c88541bdc134817777
Andrey Rogachevich <andrey.rogachevich@azati.com>
2019-07-11 11:40:16 +0300

    * 2. Object with function

--- src/code.js
+++ src/code.js
@@ -1,8 +1,13 @@
 const Animal = function (iKind, iName) {
   this.kind = iKind;
   this.name = iName;
+  this.eat = function() {
+    console.log('Хрум-хрум');
+
+  }
 };
 
 const cat = new Animal('cat', 'Bob');
+cat.eat();
 
 console.log(">>", cat);
