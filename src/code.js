00853ad4021ddb7875e11446dd32e793b1ced7f3
Andrey Rogachevich <andrey.rogachevich@azati.com>
2019-07-16 18:41:02 +0300

    * 5. Readonly object

--- src/code.js
+++ src/code.js
@@ -1,23 +1,5 @@
-const Animal = function (iKind, iName) {
-  this.kind = iKind;
-  this.name = iName;
-};
+const constObj = Object.create(null, {myProp: {value: 42}});
 
-Animal.prototype.eat = function () {
-  console.log(this.kind, this.name, 'says', 'Хрум-хрум');
-};
+constObj.myProp = 5;
 
-const Dog = function (iName) {
-  Animal.call(this, 'dog', iName);
-};
-
-Dog.prototype = Object.create(Animal.prototype);
-Dog.prototype.constructor = Dog;
-
-const woofy = new Dog('Woofy');
-woofy.eat();
-
-console.log("WOOFY >>", woofy);
-console.log("Dog? >>", woofy instanceof Dog);
-console.log("Animal? >>", woofy instanceof Animal);
-console.log("Object? >>", woofy instanceof Object);
+console.log(">>", constObj);
