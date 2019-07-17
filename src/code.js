04a636c48e6aa6c3e8f091abf3def5109e62669f
Andrey Rogachevich <andrey.rogachevich@azati.com>
2019-07-17 11:14:29 +0300

    * 6. getOwnPropertyDescriptor

--- src/code.js
+++ src/code.js
@@ -1,5 +1,18 @@
-const constObj = Object.create(null, {myProp: {value: 42}});
+const obj = {
+  _name: 'Chev',
+  _id: 2,
+  get name() {
+    return this._name;
+  },
+  set name(value) {
+    this._name = value;
+  },
+  get id() {
+    return this._id.toString(2);
+  },
 
-constObj.myProp = 5;
+};
 
-console.log(">>", constObj);
+console.log("_id >>", Object.getOwnPropertyDescriptor(obj, '_id'));
+console.log("id >>", Object.getOwnPropertyDescriptor(obj, 'id'));
+
